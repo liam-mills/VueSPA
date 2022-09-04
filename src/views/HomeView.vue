680a98a0ea8033ed947c6d1f13e5150bff420992
Liam <liammills.web@gmail.com>
2022-09-04 13:59:54 +1000

    Composition API reactivityTransform

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import TheWelcome from "@/components/TheWelcome.vue";
+
+let message = $ref('Default Message');
+
+setTimeout(() => {
+  message = 'Updated Message';
+}, 2000);
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
+    <p>
+      <input type="text" v-model="message" />
+    </p>
   </main>
 </template>
