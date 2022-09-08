1b62d5e43569dcd9d7d167a1ab959fc27308540f
Liam <liammills.web@gmail.com>
2022-09-08 19:13:44 +1000

    Local storage with ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <script setup>
-  import { useFlash } from '@/composables/useFlash';
+  import { ref } from 'vue';
 
-  let { flash } = useFlash();
+  let food = ref(localStorage.getItem('food'));
+  let age = ref(localStorage.getItem('age'));
+
+  function write(key, value) {
+    localStorage.setItem(key, value);
+  }
 </script>
 
 <template>
   <main>
     <p>
-      <button @click="flash('Title', 'Message')">Click Me</button>
+      Favourite food? <input type="text" v-model="food" @input="write('food', food)">
+    </p>
+    <p>
+      Age? <input type="text" v-model="age" @input="write('age', age)">
     </p>
   </main>
 </template>
